7ffe8f72c7978a447006424de869186e94d2202a
Roel Zeilstra <roel@sterc.nl>
2020-04-17 22:41:31 +0200

    Add bot

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = (client, member) => {
+    member.send(
+        `Welcome to the Extravaganza 1v1 tournament. Good Luck & Have Fun 😀`
+    )
+}
---
+++ events/message.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const kick = require("../commands/kick")
+module.exports = (client, message) => {
+    if (message.content === 'ping') {
+        return message.reply('pong')
+    }
+
+    if (message.content === '!tournament' || message.content === '!rules' || message.content === '!1v1'){
+        return message.reply(process.env.RULES)
+    }
+
+    if (message.content.startsWith("!kick")) {
+        return kick(message)
+    }
+
+    if (message.content.startsWith("!ruffle")) {
+        const types = process.env.CLASSES.split(',')
+        const maps  = process.env.MAPS.split(',')
+        const type  = types[Math.floor(Math.random() * types.length)];
+        const map   = maps[Math.floor(Math.random() * maps.length)];
+
+        return message.reply(
+            `The next round will be played with ${type} on the next map: ${map}`
+        )
+    }
+}
