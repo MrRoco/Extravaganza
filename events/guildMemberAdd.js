e6f8a1d56fd2199d310d2e5874cc6ace3cf68cd4
Roel Zeilstra <roel@sterc.nl>
2020-04-19 15:21:23 +0200

    Add explanation

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,6 +1,6 @@
 module.exports = (client, member) => {
     member.send(
-        `Welcome to the Extravaganza 1v1 tournament. Good Luck & Have Fun 😀`
+        `Welcome to the Extravaganza 1v1 tournament, type !commands for the commands needed to participate. Good Luck & Have Fun 😀`
     )
 
     var role= member.guild.roles.cache.find(role => role.name === "Players");
