0ed514f06814df92b5e3244fcf9c5a5d388abda9
Roel Zeilstra <roel@sterc.nl>
2020-04-19 13:51:20 +0200

    Add new rules && add role to new user.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,4 +2,7 @@ module.exports = (client, member) => {
     member.send(
         `Welcome to the Extravaganza 1v1 tournament. Good Luck & Have Fun 😀`
     )
+
+    var role= member.guild.roles.cache.find(role => role.name === "Players");
+    member.roles.add(role);
 }
