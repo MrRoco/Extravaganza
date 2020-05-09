8e2a8a01990642d9fa15dd8265d20de71563671d
Roel Zeilstra <roel@sterc.nl>
2020-05-09 12:09:23 +0200

    Add option for username

--- events/message.js
+++ events/message.js
@@ -147,6 +147,24 @@ module.exports = (client, message) => {
         return message.channel.send({embed});
     }
 
+    if (message.content === '!final-16') {
+        const roleID = "707511394026651749";
+        const membersWithRole = message.guild.roles.cache.get(roleID).members
+        const memberSize = membersWithRole.size;
+        const players = membersWithRole.map(member => {
+            return member.user.username;
+        }).sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'}));
+
+        // inside a command, event listener, etc.
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0xFFFF')
+            .setTitle('Players total: ' + memberSize)
+            .setDescription(players.join("\n"))
+            .setTimestamp();
+
+        return message.channel.send({embed});
+    }
+
     if (message.content.startsWith("!ruffle")) {
         const types = process.env.CLASSES.split(',')
         const maps  = process.env.MAPS.split(',')
@@ -159,20 +177,22 @@ module.exports = (client, message) => {
     }
 
     if (command === 'ban') {
-        if(!message.member.roles.cache.some(r=>["Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
+        if(!message.member.roles.cache.some(r=>["Original Team", "Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
             return;
         }
 
-
         const channel = message.guild.channels.cache.get('705038712417026108');
-        //const channel = client.channels.fetch(705038712417026108) as Discord.TextChannel;
-        var role = 'Group-1';
+        var role = 'Final-16';
         if(message.member.roles.cache.has('704757529241976908')) {
             role = 'Group-2';
         } else if (message.member.roles.cache.has('704757561328402432')) {
             role = 'Group-3';
         } else if (message.member.roles.cache.has('704757584472703126')) {
             role = 'Group-4';
+        }
+
+        if (args[1]) {
+            return channel.send(`${role}: ${args[1]} has blocked tier: ${args[0]}`);
         }
 
         return channel.send(`${role}: ${message.author.username} has blocked tier: ${args[0]}`);
@@ -180,19 +200,22 @@ module.exports = (client, message) => {
 
 
     if (command === 'class') {
-        if(!message.member.roles.cache.some(r=>["Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
+        if(!message.member.roles.cache.some(r=>["Original Team", "Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
             return;
         }
 
         const channel = message.guild.channels.cache.get('705038712417026108');
-        //const channel = client.channels.fetch(705038712417026108) as Discord.TextChannel;
-        var role = 'Group-1';
+        var role = 'Final-16';
         if(message.member.roles.cache.has('704757529241976908')) {
             role = 'Group-2';
         } else if (message.member.roles.cache.has('704757561328402432')) {
             role = 'Group-3';
         } else if (message.member.roles.cache.has('704757584472703126')) {
             role = 'Group-4';
+        }
+
+        if (args[1]) {
+            return channel.send(`${role}: ${args[1]} has chosen class: ${args[0]}`);
         }
 
         return channel.send(`${role}: ${message.author.username} has chosen class: ${args[0]}`);
@@ -200,7 +223,7 @@ module.exports = (client, message) => {
 
 
     if (command === 'win') {
-        if(!message.member.roles.cache.some(r=>["Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
+        if(!message.member.roles.cache.some(r=>["Original Team", "Staff", "Group-1", "Group-2", "Group-3", "Group-4"].includes(r.name)) ) {
             return;
         }
 
@@ -213,6 +236,10 @@ module.exports = (client, message) => {
             role = 'Group-3';
         } else if (message.member.roles.cache.has('704757584472703126')) {
             role = 'Group-4';
+        }
+
+        if (args[1]) {
+            return channel.send(`${role}: ${args[1]} has won with the following score: ${args[0]}`);
         }
 
         return channel.send(`${role}: ${message.author.username} has won with the following score: ${args[0]}`);
